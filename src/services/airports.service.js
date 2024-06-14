be6ff47786b5b87927efe7e11e3aad4d39442e2c
Student1-41587003 <Student1-41587003@LODSPRODMCA.onmicrosoft.com>
2024-06-14 13:16:47 -0700

    Changes airport sorting. Fixes AB#169

--- src/services/airports.service.js
+++ src/services/airports.service.js
@@ -8,10 +8,10 @@ class AirportsService {
     }
 
     getAll(){
-        getAll(){
-            return this._airports.filter(a => a.code).map(avoidEmptyCity).sort((a, b) => (a.city > b.city) ? 1 : -1);
-        }
+        return this._airports.filter(a => a.code).map(avoidEmptyCity).sort((a, b) => (a.city > b.city) ? 1 : -1);
     }
+    
+    
 
     getByCode(code) {
         return avoidEmptyCity(this._airports.find(a => a.code == code));
